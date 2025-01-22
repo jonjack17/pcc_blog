90bea5ea1ff9806d3abb52a17b08cf074036ad8e
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-01-22 17:01:36 -0500

    Created project of  blog_project and app of blogs

---
+++ blogs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# class Blog(models.Model):
+#     """A blog that the user is writing."""
+#     text = models.CharField(max_length=200)
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         """Return a string representation of the model."""
+#         return self.text
+
+# class Post(models.Model):
+#     """A specific post within a blog."""
+#     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         verbose_name_plural = 'posts'
+    
+#     def __str__(self):
+#         """Return a simple string representing the entry."""
+#         return f"{self.text[:50]}..."
