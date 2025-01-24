e67e7e46d666e5c83f6c4e072c2c0a53f595cadd
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-01-24 17:42:38 -0500

    Continued blog project
    User can now add a blog or a post to a specific blog. next and final step is to add functionality for editing an existing post.

--- blogs/urls.py
+++ blogs/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('blogs/', views.blogs, name='blogs'),
     # Page that lists a specific blog and its posts.
     path('blogs/<int:blog_id>/', views.blog, name='blog'),
+    # Page for creating a new blog.
+    path('new_blog/', views.new_blog, name='new_blog'),
+    # Page for creating a new post for a specific blog.
+    path('new_post/<int:blog_id>/', views.new_post, name ='new_post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Blog, Post
+from .forms import BlogForm, PostForm
 
 def index(request):
     """The home page for Blog."""
@@ -18,3 +19,40 @@ def blog(request, blog_id):
     posts = blog.post_set.order_by('-date_added')
     context = {'blog':blog, 'posts':posts}
     return render(request, 'blogs/blog.html', context)
+
+def new_blog(request):
+    """Page for creating a new blog."""
+    if request.method != 'POST':
+        # No data submitted, create a blank form.
+        form = BlogForm()
+    else:
+        # POST data submitted; process data.
+        form = BlogForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogs:blogs')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'blogs/new_blog.html',context)
+
+def new_post(request, blog_id):
+    """Page for creating a new post for a specific blog."""
+    blog = Blog.objects.get(id=blog_id)
+
+    if request.method != 'POST':
+        # No data submitted, create a blank form.
+        form = PostForm()
+    else:
+        # POST data submitted; process data.
+        form = PostForm(data=request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.blog = blog
+            new_post.save()
+            return redirect('blogs:blog', blog_id=blog_id)
+        
+    # Display a blank or invalid form.
+    context = {'blog':blog, 'form': form}
+    return render(request, 'blogs/new_post.html', context)
+
