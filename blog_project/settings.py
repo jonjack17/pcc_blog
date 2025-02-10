04a6f3d3ee80b0478538a4bf3ea40c3aed993490
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-02-10 18:32:14 -0500

    Began updating base.html with bootstrap
    headers finished, next is nav bar section

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     # My apps.
     'blogs',
     'accounts',
+
+    # Third party apps.
+    'django_bootstrap5',
      
     # Default django apps.
     'django.contrib.admin',
