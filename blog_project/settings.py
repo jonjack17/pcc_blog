37addd13986d049d51d6933668b780a724e08440
jonjack17 <151687987+jonjack17@users.noreply.github.com>
2025-02-17 11:09:12 -0500

    Added custom error pages and changed DEBUG to False

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,6 +141,7 @@ from platformshconfig import Config
 config = Config()
 if config.is_valid_platform():
     ALLOWED_HOSTS.append('.platformsh.site')
+    DEBUG = False
 
     if config.appDir:
         STATIC_ROOT = Path(config.appDir) / 'static'
